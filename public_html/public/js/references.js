b04cac2d0991de814814e9d999b745a04d6429ad
Erik Ibolya <erikibolya@gmail.com>
2019-09-17 09:01:43 +0200

    Final

--- public_html/public/js/references.js
+++
@@ -1,8 +0,0 @@
-var groups = document.querySelectorAll(".references__group");
-var button = document.querySelector(".references__see-more");
-
-console.log(button);
-
-button.addEventListener("click", function (e) {
-    console.log("a");
-}, false);
